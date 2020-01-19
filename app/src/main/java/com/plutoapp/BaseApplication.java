95a6e3e265c425b937ca280d31b22491fbd753f3
Saket Patel <saket.is.sam@gmail.com>
2020-01-20 00:24:14 +0530

    Added basic file structure and Dagger for DI

---
+++ app/src/main/java/com/plutoapp/BaseApplication.java
@@ -0,0 +1,14 @@
+package com.plutoapp;
+
+import com.plutoapp.components.DaggerAppComponent;
+
+import dagger.android.AndroidInjector;
+import dagger.android.support.DaggerApplication;
+
+public class BaseApplication extends DaggerApplication {
+
+    @Override
+    protected AndroidInjector<? extends DaggerApplication> applicationInjector() {
+        return DaggerAppComponent.builder().application(this).build();
+    }
+}
---
+++ app/src/main/java/com/plutoapp/components/AppComponent.java
@@ -0,0 +1,28 @@
+package com.plutoapp.components;
+
+import android.app.Application;
+
+import com.plutoapp.BaseApplication;
+
+import dagger.BindsInstance;
+import dagger.Component;
+import dagger.android.AndroidInjector;
+import dagger.android.support.AndroidSupportInjectionModule;
+
+@Component(
+        modules = {
+                AndroidSupportInjectionModule.class,
+                ActivityBuildersModule.class
+        }
+)
+public interface AppComponent extends AndroidInjector<BaseApplication> {
+
+    @Component.Builder
+    interface Builder {
+
+        @BindsInstance
+        Builder application(Application application);
+
+        AppComponent build();
+    }
+}
